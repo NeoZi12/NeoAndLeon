3ff41fa159228f9609f8b3798927ec052701e972
NeoZi12 <neozi2014@gmail.com>
2025-04-20 15:26:21 +0300

    Add files via upload

--- eventy.sql
+++ eventy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 20, 2025 at 02:23 PM
+-- Generation Time: Apr 20, 2025 at 02:25 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
